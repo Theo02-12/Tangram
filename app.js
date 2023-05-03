ea8a1c852b4c2691a346ce7a29377c2671194482
Theo02-12 <mrcouvelard@gmail.com>
2023-05-03 21:09:46 +0200

    oui

--- app.js
+++ app.js
@@ -3,16 +3,27 @@ const figureDiv = document.querySelectorAll('.figure');
 
 const btnCarre = document.getElementById('carre');
 const btnGiraffe = document.getElementById('giraffe')
+const btnVautour = document.getElementById('vautour');
+
 
 btnCarre.addEventListener('click', () =>{
     figureDiv.forEach(figures => {
         figures.classList.remove('carre');
         figures.classList.remove('giraffe');
+        figures.classList.remove('vautour');
     })
 })
 btnGiraffe.addEventListener('click', () =>{
     figureDiv.forEach(figures => {
         figures.classList.add('giraffe');
         figures.classList.remove('carre');
+        figures.classList.remove('vautour');
+    })
+})
+btnVautour.addEventListener('click', () =>{
+    figureDiv.forEach(figures => {
+        figures.classList.add('vautour');
+        figures.classList.remove('carre');
+        figures.classList.remove('giraffe');
     })
 })
